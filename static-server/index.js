f4e8584de0445b0384b3a3104f4117aa83deb62d
madhavsharma2106 <madhavsharma2106@gmail.com>
2020-07-10 21:20:45 +0530

    added static server

---
+++ static-server/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 80;
+
+// Serve any static files
+app.use(express.static(path.join(__dirname, "../build")));
+
+// Handle React routing, return all requests to React app
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`APP launched on ${PORT}`);
+});
